6eceb87bc7bbd55424df252530545b4a92947357
sahil.chug <sahil.chug@fireeye.com>
2016-12-07 13:51:31 +0530

    Refactor tests

--- articles/tests.py
+++ articles/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from .models import Article
+
 # Create your tests here.
 
 
@@ -9,28 +10,23 @@ class TestArticle(TestCase):
     Test class to test Article model
     """
 
-    def setUp(TestCase):
-        Article.objects.create(title='test', author='admin')
+    def setUp(self):
+        self.article = Article.objects.create(title='test', author='admin')
 
     def test_article_sets_title(self):
-        article = Article.objects.get()
-        self.assertEqual(article.title, 'test')
+        self.assertEqual(self.article.title, 'test')
 
     def test_article_sets_author(self):
-        article = Article.objects.get()
-        self.assertEqual(article.author, 'admin')
+        self.assertEqual(self.article.author, 'admin')
 
     def test_article_sets_publication_date(self):
-        article = Article.objects.get()
-        self.assertEqual(article.publication_date, '')
+        self.assertEqual(self.article.publication_date, '')
 
     def test_article_sets_category(self):
-        article = Article.objects.get()
-        self.assertEqual(article.category, '')
+        self.assertEqual(self.article.category, '')
 
     def test_article_sets_body(self):
-        article = Article.objects.get()
-        self.assertEqual(article.body, '')
+        self.assertEqual(self.article.body, '')
 
     def test_article_sets_hero_image(self):
         #TODO filgure out how to test ImageField
