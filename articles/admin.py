f4d5128e7670b6c89aa6d67699fac1a881bbcff4
sahil.chug <sahil.chug@fireeye.com>
2016-12-07 17:25:00 +0530

    Register Article and Category to Admin portal

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Article, Category
 # Register your models here.
+
+
+@admin.register(Article, Category)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
