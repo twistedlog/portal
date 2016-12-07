1a125b87dde7f37c6bd7a4172aab75266ab9e9dd
sahil.chug <sahil.chug@fireeye.com>
2016-12-07 12:10:24 +0530

    Add tests to test title field is set in Article

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from .models import Article
 # Create your tests here.
+
+
+class TestArticles(TestCase):
+    """
+    Test class to test Article model
+    """
+
+    def setUp(TestCase):
+        Article.objects.create(title='test')
+
+    def test_article_sets_title(self):
+        article = Article.objects.get()
+        self.assertEqual(article.title, 'test')
