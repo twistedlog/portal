ce45d7fd3389f6c229682ef80bb12b7835f4e611
sahil.chug <sahil.chug@fireeye.com>
2016-12-07 13:47:26 +0530

    Add tests for rest of the fields

--- articles/tests.py
+++ articles/tests.py
@@ -19,3 +19,23 @@ class TestArticle(TestCase):
     def test_article_sets_author(self):
         article = Article.objects.get()
         self.assertEqual(article.author, 'admin')
+
+    def test_article_sets_publication_date(self):
+        article = Article.objects.get()
+        self.assertEqual(article.publication_date, '')
+
+    def test_article_sets_category(self):
+        article = Article.objects.get()
+        self.assertEqual(article.category, '')
+
+    def test_article_sets_body(self):
+        article = Article.objects.get()
+        self.assertEqual(article.body, '')
+
+    def test_article_sets_hero_image(self):
+        #TODO filgure out how to test ImageField
+        pass
+
+    def test_article_sets_optional_image(self):
+        #TODO filgure out how to test ImageField
+        pass
