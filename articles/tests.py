9f50d739fbfb915cf7711eaae895fdc427e68b5f
sahil.chug <sahil.chug@fireeye.com>
2016-12-07 13:30:15 +0530

    Add test to check author of an article

--- articles/tests.py
+++ articles/tests.py
@@ -4,14 +4,18 @@ from .models import Article
 # Create your tests here.
 
 
-class TestArticles(TestCase):
+class TestArticle(TestCase):
     """
     Test class to test Article model
     """
 
     def setUp(TestCase):
-        Article.objects.create(title='test')
+        Article.objects.create(title='test', author='admin')
 
     def test_article_sets_title(self):
         article = Article.objects.get()
         self.assertEqual(article.title, 'test')
+
+    def test_article_sets_author(self):
+        article = Article.objects.get()
+        self.assertEqual(article.author, 'admin')
