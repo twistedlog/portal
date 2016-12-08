562b4b41f7d18116f894f74d3b014d4edd715f34
sahil.chug <sahil.chug@fireeye.com>
2016-12-08 13:17:11 +0530

    Add Templates and Templaet DIRS in setting

--- portal/urls.py
+++ portal/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^article/', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
