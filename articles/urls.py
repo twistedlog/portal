b5668dd8765b672a9586b45ab46532f0c405b186
sahil.chug <sahil.chug@fireeye.com>
2016-12-08 15:07:21 +0530

    Added a basic template and some initial angular code

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
 from .views import ArticleListView
 
 
 urlpatterns = [
-    url(r'list/$', ArticleListView.as_view(), name='article-list')
+    url(r'list/$', ArticleListView.as_view(), name='article-list'),
+    url(r'list/html/$', TemplateView.as_view(template_name='article_list.html'))
 ]
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,20 @@
 (function() {
   var app = angular.module('myApp', [])
 
+  app.controller('articleListController',['$http', '$log', function($http, $log){
+    site = this;
+    site.articles = [];
+    $http.get('/article/list/').success(function(data){
+    	site.articles = data;
+    });
+
+  }]);
+
+  app.directive('articleList', function(){
+	  	return {
+	  		restrict: 'E',
+	  		templateUrl: '/article/list/html/'
+	  	}
+	});
+
 })();
